5754e4cb698884b942487ebd25dbaee091f38533
toxigeno <melissaaraya@yahoo.com>
2021-03-13 21:43:51 +0000

    primer link creado

---
+++ src/js/views/contact.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Contact = () => (
+	<div className="text-center mt-5">
+		<h1>Contact Page</h1>
+	</div>
+);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
+import { Link } from "react-router-dom";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<Link to="/contact" className="btn btn-danger">
+			Contact
+		</Link>
+		<h1>Me cago en Rigo!</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
