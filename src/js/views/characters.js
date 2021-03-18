109a25cbd92316bc14a7041c8f62771388342d29
toxigeno <melissaaraya@yahoo.com>
2021-03-18 05:24:06 +0000

    2 views

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -2,13 +2,17 @@ import React from "react";
 import Card from "../component/card";
 
 export const Characters = () => (
-	<div className="row">
-		<div className="col">
-			<div className="text-center mt-5">
-				<h1>Characters</h1>
-				<Card title="personajes" />
-				<Card title="planetas" />
-				<Card title="vehiculos" />
+	<div className="text-center mt-5">
+		<h1>Characters</h1>
+		<div className="row">
+			<div className="col">
+				<Card title="Han Solo" />
+			</div>
+			<div className="col">
+				<Card title="Chewbacca" />
+			</div>
+			<div className="col">
+				<Card title="Dart Vader" />
 			</div>
 		</div>
 	</div>
--- src/js/views/planets.js
+++ src/js/views/planets.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Card from "../component/card";
 
 export const Planets = () => (
 	<div className="text-center mt-5">
 		<h1>Planets</h1>
+		<div className="row">
+			<div className="col">
+				<Card title="Dandoran" />
+			</div>
+			<div className="col">
+				<Card title="Utapau" />
+			</div>
+			<div className="col">
+				<Card title="Yarma" />
+			</div>
+		</div>
 	</div>
 );
