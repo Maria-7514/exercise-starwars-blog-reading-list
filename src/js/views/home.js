0fd37f566f5ad26ff5161668b1b44eb57cc977fd
toxigeno <melissaaraya@yahoo.com>
2021-03-18 06:22:47 +0000

    cards

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "../../styles/home.scss";
 import { Link } from "react-router-dom";
+import { Characters } from "./characters";
+import { Planets } from "./planets";
 
 export const Home = () => (
 	<div className="container">
 		<div className="row mt-1 d-md-block">
-			<Link to="/characters" className="btn btn-danger btn-lg">
-				Characters
-			</Link>
+			<Characters />
 		</div>
 		<div className="row mt-1 d-md-block">
-			<Link to="/planets" className="btn btn-danger btn-lg">
-				Planets
-			</Link>
-			{/* <Link to="/demo">
-			<button className="btn btn-primary">Check the Context in action</button>
-		</Link> */}
+			<Planets />
 		</div>
 	</div>
 );
